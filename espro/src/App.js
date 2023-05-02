fc5622a2e412dee6b0e6437a7d0475ac7d42f602
josip <josiprados.biz@gmail.com>
2023-05-02 13:44:36 +0200

    feat: started bestSellers seciton

--- espro/src/App.js
+++ espro/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
+import BestSellers from './components/bestsellers/BestSellers';
+
+import bestSellersArray from "./components/bestsellers/bestSellersArray.json"
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Hero></Hero>
+      <BestSellers
+      array={bestSellersArray}
+      ></BestSellers>
     </div>
   );
 }
---
+++ espro/src/components/bestsellers/BestSeller.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function BestSeller({element}) {
+
+  return (
+    <div>
+        <p>
+            {element.name}
+        </p>
+    </div>
+  )
+}
+
+export default BestSeller;
